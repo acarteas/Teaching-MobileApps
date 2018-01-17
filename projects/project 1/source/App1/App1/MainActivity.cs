24f00e77761c4ed542c0a942195390d3df39b934
zeefree <zfreeman997@gmail.com>
2018-01-17 14:53:05 -0800

    Was previously running visual studio, should now have added the HelloWolrdesque app

--- projects/project 1/source/App1/App1/MainActivity.cs
+++ projects/project 1/source/App1/App1/MainActivity.cs
@@ -20,8 +20,10 @@ namespace App1
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
+
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
         }
     }
+
 }
 
