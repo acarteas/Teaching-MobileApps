667cae3d8db417a606add6714281866955565cce
leviwp48 <leviwp48@gmail.com>
2018-01-23 12:21:51 -0800

    Added Grid Layout

---
+++ projects/project 1/source/CalculatorApp/CalculatorApp/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace CalculatorApp
+{
+    [Activity(Label = "CalculatorApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 0;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            var label = FindViewById<TextView>(Resource.Id.textView1);
+            button.Click += delegate 
+            {
+                count++;
+                label.Text = $"You clicked {count} times";
+
+            };
+        }
+      
+    }
+}
+
