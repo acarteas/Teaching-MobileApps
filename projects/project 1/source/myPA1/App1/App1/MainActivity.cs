acf846d4c3c41185600f448b850d40ba38feb2a8
Alec <arl505@humboldt.edu>
2018-01-22 16:47:37 -0800

    add myPA1

---
+++ projects/project 1/source/myPA1/App1/App1/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App1
+{
+    [Activity(Label = "Character Counter", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+        }
+    }
+}
+
---
+++ projects/project 1/source/myPA1/App2/App2/MainActivity.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Character_Counter
+{
+    [Activity(Label = "Character Counter", MainLauncher = true, Icon = "@mipmap/icon")]
+
+    public class MainActivity : Activity
+    {
+
+    }
+}
+
