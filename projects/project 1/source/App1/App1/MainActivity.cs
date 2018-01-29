23bb8a6e4ac31746d040ebeaf0ef3f2a59dcf475
Julian <jjl438@humboldt.edu>
2018-01-28 17:44:57 -0800

    New work

--- projects/project 1/source/App1/App1/MainActivity.cs
+++ projects/project 1/source/App1/App1/MainActivity.cs
@@ -4,10 +4,12 @@ using Android.OS;
 
 namespace App1
 {
-    [Activity(Label = "App1-Postfix Calculator", MainLauncher = true, Icon = "@mipmap/icon")]
+    [Activity(Label = "Postfix Calculator", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        string str_input = null;
+        string str_output = null;
+        string input_box; 
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -18,25 +20,145 @@ namespace App1
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button1  = FindViewById<Button>(Resource.Id.button1);
-            Button button2 = FindViewById<Button>(Resource.Id.button2);
-            Button button3 = FindViewById<Button>(Resource.Id.button3);
-            Button button4 = FindViewById<Button>(Resource.Id.button4);
-            Button button5 = FindViewById<Button>(Resource.Id.button5);
-            Button button6 = FindViewById<Button>(Resource.Id.button6);
-            Button button7 = FindViewById<Button>(Resource.Id.button7);
-            Button button8 = FindViewById<Button>(Resource.Id.button8);
-            Button button9 = FindViewById<Button>(Resource.Id.button9);
-            Button button10 = FindViewById<Button>(Resource.Id.button10);
-            Button button11 = FindViewById<Button>(Resource.Id.button11);
-            Button button12 = FindViewById<Button>(Resource.Id.button12);
-            Button button13 = FindViewById<Button>(Resource.Id.button13);
-            Button button14 = FindViewById<Button>(Resource.Id.button14);
-            Button button15 = FindViewById<Button>(Resource.Id.button15);
-            Button button16 = FindViewById<Button>(Resource.Id.button16);
+            Button button1  = FindViewById<Button>(Resource.Id.Button1);
+            Button button2 = FindViewById<Button>(Resource.Id.Button2);
+            Button button3 = FindViewById<Button>(Resource.Id.Button3);
+            Button button4 = FindViewById<Button>(Resource.Id.Button4);
+            Button button5 = FindViewById<Button>(Resource.Id.Button5);
+            Button button6 = FindViewById<Button>(Resource.Id.Button6);
+            Button button7 = FindViewById<Button>(Resource.Id.Button7);
+            Button button8 = FindViewById<Button>(Resource.Id.Button8);
+            Button button9 = FindViewById<Button>(Resource.Id.Button9);
+            Button button0 = FindViewById<Button>(Resource.Id.Button0);
+            Button buttonClear = FindViewById<Button>(Resource.Id.ButtonClear);
+            Button buttonADD = FindViewById<Button>(Resource.Id.ButtonPlus);
+            Button buttonSubtract = FindViewById<Button>(Resource.Id.ButtonMinus);
+            Button buttonMultiply = FindViewById<Button>(Resource.Id.ButtonMultiply);
+            Button buttonDivide = FindViewById<Button>(Resource.Id.ButtonDivide);
+            Button buttonInvert = FindViewById<Button>(Resource.Id.ButtonInvert);
+            
+            var input_box = FindViewById(Resource.Id.calculatorbox);
 
 
-            button1.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button1.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '1';
+                this.str_output += '1';
+                input_box.Text = this.str_output; 
+            };
+
+            button2.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '2';
+                this.str_output += '2';
+                input_box.Text = this.str_output;
+            };
+
+            button3.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '3';
+                this.str_output += '3';
+                input_box.Text = this.str_output;
+            };
+
+            button4.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '4';
+                this.str_output += '4';
+                input_box.Text = this.str_output;
+            };
+
+            button5.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '5';
+                this.str_output += '5';
+                input_box.Text = this.str_output;
+            };
+
+            button6.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '6';
+                this.str_output += '6';
+                input_box.Text = this.str_output;
+            };
+
+            button7.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '7';
+                this.str_output += '7';
+                input_box.Text = this.str_output;
+            };
+
+            button8.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '8';
+                this.str_output += '8';
+                input_box.Text = this.str_output;
+            };
+
+            button9.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '9';
+                this.str_output += '9';
+                input_box.Text = this.str_output;
+            };
+
+            button0.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '0';
+                this.str_output += '0';
+                input_box.Text = this.str_output;
+            };
+
+            buttonClear.Click += delegate
+            {
+                myVib.Vibrate(25);
+                input_box.Text = null;
+            };
+
+            buttonADD.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '1';
+                this.str_output += '1';
+                input_box.Text = this.str_output;
+            };
+
+            buttonSubtract.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '1';
+                this.str_output += '1';
+                input_box.Text = this.str_output;
+            };
+
+            buttonDivide.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '1';
+                this.str_output += '1';
+                input_box.Text = this.str_output;
+            };
+
+            buttonInvert.Click += delegate
+            {
+                myVib.Vibrate(25);
+                this.str_input += '1';
+                this.str_output += '1';
+                input_box.Text = this.str_output;
+            };
+
+
         }
     }
 }
--- projects/project 1/source/App1/App1/Resources/Resource.Designer.cs
+++ projects/project 1/source/App1/App1/Resources/Resource.Designer.cs
@@ -44,53 +44,68 @@ namespace App1
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int button1 = 2131034112;
-			
-			// aapt resource value: 0x7f050009
-			public const int button10 = 2131034121;
-			
-			// aapt resource value: 0x7f05000a
-			public const int button11 = 2131034122;
-			
-			// aapt resource value: 0x7f05000b
-			public const int button12 = 2131034123;
-			
-			// aapt resource value: 0x7f05000c
-			public const int button13 = 2131034124;
+			// aapt resource value: 0x7f050012
+			public const int Button0 = 2131034130;
 			
 			// aapt resource value: 0x7f05000d
-			public const int button14 = 2131034125;
+			public const int Button1 = 2131034125;
 			
 			// aapt resource value: 0x7f05000e
-			public const int button15 = 2131034126;
+			public const int Button2 = 2131034126;
 			
 			// aapt resource value: 0x7f05000f
-			public const int button16 = 2131034127;
+			public const int Button3 = 2131034127;
 			
-			// aapt resource value: 0x7f050001
-			public const int button2 = 2131034113;
+			// aapt resource value: 0x7f050009
+			public const int Button4 = 2131034121;
 			
-			// aapt resource value: 0x7f050002
-			public const int button3 = 2131034114;
+			// aapt resource value: 0x7f05000a
+			public const int Button5 = 2131034122;
 			
-			// aapt resource value: 0x7f050003
-			public const int button4 = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int button5 = 2131034116;
+			// aapt resource value: 0x7f05000b
+			public const int Button6 = 2131034123;
 			
 			// aapt resource value: 0x7f050005
-			public const int button6 = 2131034117;
+			public const int Button7 = 2131034117;
 			
 			// aapt resource value: 0x7f050006
-			public const int button7 = 2131034118;
+			public const int Button8 = 2131034118;
 			
 			// aapt resource value: 0x7f050007
-			public const int button8 = 2131034119;
+			public const int Button9 = 2131034119;
+			
+			// aapt resource value: 0x7f050002
+			public const int ButtonClear = 2131034114;
+			
+			// aapt resource value: 0x7f050013
+			public const int ButtonDecimal = 2131034131;
+			
+			// aapt resource value: 0x7f050003
+			public const int ButtonDelete = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int ButtonDivide = 2131034116;
+			
+			// aapt resource value: 0x7f050014
+			public const int ButtonEqual = 2131034132;
+			
+			// aapt resource value: 0x7f050011
+			public const int ButtonInvert = 2131034129;
+			
+			// aapt resource value: 0x7f05000c
+			public const int ButtonMinus = 2131034124;
 			
 			// aapt resource value: 0x7f050008
-			public const int button9 = 2131034120;
+			public const int ButtonMultiply = 2131034120;
+			
+			// aapt resource value: 0x7f050010
+			public const int ButtonPlus = 2131034128;
+			
+			// aapt resource value: 0x7f050000
+			public const int MainPageLayout = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int calculatorbox = 2131034113;
 			
 			static Id()
 			{
