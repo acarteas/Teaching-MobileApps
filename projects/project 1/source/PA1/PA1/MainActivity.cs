5753b6e500753b1ec1fe81b524fafc549c237e84
zeefree <zfreeman997@gmail.com>
2018-01-29 16:51:30 -0800

    Added basics so buttons actually edit text field

--- projects/project 1/source/PA1/PA1/MainActivity.cs
+++ projects/project 1/source/PA1/PA1/MainActivity.cs
@@ -1,21 +1,34 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
 
 namespace PA1
 {
     [Activity(Label = "PA1", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            Button b1 = FindViewById<Button>(Resource.Id.onebutton);
+            Button b2 = FindViewById<Button>(Resource.Id.twobutton);
+            Button b3 = FindViewById<Button>(Resource.Id.threebutton);
+            Button b4 = FindViewById<Button>(Resource.Id.fourbutton);
+            Button b5 = FindViewById<Button>(Resource.Id.fivebutton);
+            Button b6 = FindViewById<Button>(Resource.Id.sixbutton);
+            Button b7 = FindViewById<Button>(Resource.Id.sevenbutton);
+            Button b8 = FindViewById<Button>(Resource.Id.eightbutton);
+            Button b9 = FindViewById<Button>(Resource.Id.ninebutton);
+            Button b0 = FindViewById<Button>(Resource.Id.zerobutton);
 
+
+            b1.Click += AddNum;
             // Get our button from the layout resource,
             // and attach an event to it
             /*
@@ -23,7 +36,18 @@ namespace PA1
 
                button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
             */
-           }
-       }
-   }
+           
+        }
+
+        public void AddNum(object sender, EventArgs e)
+        {
+            Button the_button = sender as Button;
+            TextView to_edit = FindViewById<TextView>(Resource.Id.IOtext);
+            to_edit.Text = to_edit.Text + the_button.Text;
+        }
+
+    }
+    
+}   
+  
 
