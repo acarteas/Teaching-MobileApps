2c10fcd505e2eba91d3b0f3632d67e1b362c0446
zeefree <zfreeman997@gmail.com>
2018-01-29 21:47:29 -0800

    All number buttons add to text, progress made for math oeprations

--- projects/project 1/source/PA1/PA1/MainActivity.cs
+++ projects/project 1/source/PA1/PA1/MainActivity.cs
@@ -3,19 +3,29 @@ using Android.App;
 using Android.Widget;
 using Android.OS;
 using Android.Views;
+//Needed for  CultureInfo so I can parse floats using . instead of ,
+using System.Globalization;
+
+
 
 namespace PA1
 {
+    
     [Activity(Label = "PA1", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        
+        char cur_math_expr = ' ';
+        float last_num = 0;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
+
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+
             Button b1 = FindViewById<Button>(Resource.Id.onebutton);
             Button b2 = FindViewById<Button>(Resource.Id.twobutton);
             Button b3 = FindViewById<Button>(Resource.Id.threebutton);
@@ -26,9 +36,44 @@ namespace PA1
             Button b8 = FindViewById<Button>(Resource.Id.eightbutton);
             Button b9 = FindViewById<Button>(Resource.Id.ninebutton);
             Button b0 = FindViewById<Button>(Resource.Id.zerobutton);
+            Button bdot = FindViewById<Button>(Resource.Id.zerobutton);
 
+            Button bclear = FindViewById<Button>(Resource.Id.clearbutton);
+
+            Button badd = FindViewById<Button>(Resource.Id.plusbutton);
+            Button bsub = FindViewById<Button>(Resource.Id.minusbutton);
+            Button bdivide = FindViewById<Button>(Resource.Id.dividebutton);
+            Button bmultiply = FindViewById<Button>(Resource.Id.multiplybutton);
+
+            Button bnegate = FindViewById<Button>(Resource.Id.negatebutton);
+
+            Button bequal = FindViewById<Button>(Resource.Id.equalbutton);
+            
+            
 
             b1.Click += AddNum;
+            b2.Click += AddNum;
+            b3.Click += AddNum;
+            b4.Click += AddNum;
+            b5.Click += AddNum;
+            b6.Click += AddNum;
+            b7.Click += AddNum;
+            b8.Click += AddNum;
+            b9.Click += AddNum;
+            b0.Click += AddNum;
+
+
+            bclear.Click += ClearText;
+
+            badd.Click += StoreMathExpr;
+            bsub.Click += StoreMathExpr;
+            bdivide.Click += StoreMathExpr;
+            bmultiply.Click += StoreMathExpr;
+
+            bequal.Click += EqualTime;
+
+            bnegate.Click += Negate;
+
             // Get our button from the layout resource,
             // and attach an event to it
             /*
@@ -36,14 +81,100 @@ namespace PA1
 
                button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
             */
-           
+
+        }
+
+        public void Negate(object sender, EventArgs e)
+        {
+            TextView to_edit = FindViewById<TextView>(Resource.Id.IOtext);
+            if(to_edit.Text[0] != '-')
+            {
+                to_edit.Text = "-" + to_edit.Text;
+            }
+            else
+            {
+                to_edit.Text = to_edit.Text.Substring(1);
+            }
+            
         }
 
         public void AddNum(object sender, EventArgs e)
         {
             Button the_button = sender as Button;
             TextView to_edit = FindViewById<TextView>(Resource.Id.IOtext);
-            to_edit.Text = to_edit.Text + the_button.Text;
+            if(cur_math_expr == '=')
+            {
+                to_edit.Text = the_button.Text;
+            }
+            else
+            {
+                to_edit.Text = to_edit.Text + the_button.Text;
+            }
+           
+        }
+
+        public void ClearText(object sender, EventArgs e)
+        {
+            Button the_button = sender as Button;
+            TextView to_edit = FindViewById<TextView>(Resource.Id.IOtext);
+            to_edit.Text = "0";
+        }
+
+        public void EqualTime(object sender, EventArgs e)
+        {
+            TextView to_edit = FindViewById<TextView>(Resource.Id.IOtext);
+            to_edit.Text = "";
+        }
+
+        public void StoreMathExpr(object sender, EventArgs e)
+        {
+            Button expr_button = sender as Button;
+            TextView to_edit = FindViewById<TextView>(Resource.Id.IOtext);
+            float cur_num = float.Parse(to_edit.Text, CultureInfo.InvariantCulture.NumberFormat);
+            //If we are already storing a number we need to calculate
+            if (cur_math_expr != ' ')
+            {
+                //we can assume that last_num is also being used
+                //In which case do math
+                float temp = Calculation(last_num, cur_num, cur_math_expr);
+            }
+            else
+            {
+                last_num = cur_num;
+                to_edit.Text = "0";
+                //store the cur_math_expr
+              
+            }
+            cur_math_expr = expr_button.Text[0];
+
+
+
+        }
+
+        public float Calculation(float num1, float num2, char op)
+        {
+            float num_to_return = 0;
+            switch (op)
+            {
+                case '+':
+                    num_to_return = num1 + num2;
+                    break;
+                case '-':
+                    num_to_return = num1 - num2;
+                    break;
+                case '/':
+                    num_to_return = num1 / num2;
+                    break;
+                case '*':
+                    num_to_return = num1 * num2;
+                    break;
+                default:
+                    break;
+
+            }
+
+
+            return 0;
         }
 
     }
--- projects/project 1/source/PA1/PA1/Resources/Resource.Designer.cs
+++ projects/project 1/source/PA1/PA1/Resources/Resource.Designer.cs
@@ -41,68 +41,87 @@ namespace PA1
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int button_background = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int IOtext = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int IOtext = 2131099649;
 			
-			// aapt resource value: 0x7f050013
-			public const int answerbutton = 2131034131;
+			// aapt resource value: 0x7f060011
+			public const int answerbutton = 2131099665;
 			
-			// aapt resource value: 0x7f05000e
-			public const int clear = 2131034126;
+			// aapt resource value: 0x7f060002
+			public const int clearbutton = 2131099650;
 			
-			// aapt resource value: 0x7f050005
-			public const int dividebutton = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int dividebutton = 2131099653;
 			
-			// aapt resource value: 0x7f05000b
-			public const int eightbutton = 2131034123;
+			// aapt resource value: 0x7f060012
+			public const int dotbutton = 2131099666;
 			
-			// aapt resource value: 0x7f050012
-			public const int equalbutton = 2131034130;
+			// aapt resource value: 0x7f06000f
+			public const int eightbutton = 2131099663;
 			
-			// aapt resource value: 0x7f050007
-			public const int fivebutton = 2131034119;
+			// aapt resource value: 0x7f060014
+			public const int equalbutton = 2131099668;
 			
-			// aapt resource value: 0x7f050006
-			public const int fourbutton = 2131034118;
+			// aapt resource value: 0x7f06000b
+			public const int fivebutton = 2131099659;
 			
-			// aapt resource value: 0x7f05000d
-			public const int minusbutton = 2131034125;
+			// aapt resource value: 0x7f06000a
+			public const int fourbutton = 2131099658;
 			
-			// aapt resource value: 0x7f050009
-			public const int multiplybutton = 2131034121;
+			// aapt resource value: 0x7f06000d
+			public const int minusbutton = 2131099661;
 			
-			// aapt resource value: 0x7f050010
-			public const int negatebutton = 2131034128;
+			// aapt resource value: 0x7f060009
+			public const int multiplybutton = 2131099657;
 			
-			// aapt resource value: 0x7f05000c
-			public const int ninebutton = 2131034124;
+			// aapt resource value: 0x7f060003
+			public const int negatebutton = 2131099651;
 			
-			// aapt resource value: 0x7f050002
-			public const int onebutton = 2131034114;
+			// aapt resource value: 0x7f060010
+			public const int ninebutton = 2131099664;
 			
-			// aapt resource value: 0x7f050011
-			public const int plusbutton = 2131034129;
+			// aapt resource value: 0x7f060006
+			public const int onebutton = 2131099654;
 			
-			// aapt resource value: 0x7f05000a
-			public const int sevenbutton = 2131034122;
+			// aapt resource value: 0x7f060004
+			public const int plusbutton = 2131099652;
 			
-			// aapt resource value: 0x7f050008
-			public const int sixbutton = 2131034120;
+			// aapt resource value: 0x7f06000e
+			public const int sevenbutton = 2131099662;
 			
-			// aapt resource value: 0x7f050000
-			public const int tableLayout1 = 2131034112;
+			// aapt resource value: 0x7f06000c
+			public const int sixbutton = 2131099660;
 			
-			// aapt resource value: 0x7f050004
-			public const int threebutton = 2131034116;
+			// aapt resource value: 0x7f060000
+			public const int tableLayout1 = 2131099648;
 			
-			// aapt resource value: 0x7f050003
-			public const int twobutton = 2131034115;
+			// aapt resource value: 0x7f060008
+			public const int threebutton = 2131099656;
 			
-			// aapt resource value: 0x7f05000f
-			public const int zerobutton = 2131034127;
+			// aapt resource value: 0x7f060007
+			public const int twobutton = 2131099655;
+			
+			// aapt resource value: 0x7f060013
+			public const int zerobutton = 2131099667;
 			
 			static Id()
 			{
@@ -117,8 +136,8 @@ namespace PA1
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Main = 2130968576;
 			
 			static Layout()
 			{
@@ -133,8 +152,8 @@ namespace PA1
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Icon = 2130903040;
 			
 			static Mipmap()
 			{
@@ -149,11 +168,11 @@ namespace PA1
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int hello = 2131034112;
 			
 			static String()
 			{
