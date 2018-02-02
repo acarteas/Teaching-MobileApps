251ef3dd8e9d5c34cba19d95fa61101e7195c6ad
leviwp48 <leviwp48@gmail.com>
2018-02-02 12:31:30 -0800

    Added imageview and listview to second layout

--- projects/project 2/source/ImageApp/ImageApp/Resources/Resource.Designer.cs
+++ projects/project 2/source/ImageApp/ImageApp/Resources/Resource.Designer.cs
@@ -41,6 +41,34 @@ namespace ImageApp
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040001
+			public const int imageView1 = 2130968577;
+			
+			// aapt resource value: 0x7f040003
+			public const int launchCameraButton = 2130968579;
+			
+			// aapt resource value: 0x7f040000
+			public const int linearLayout1 = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int listView1 = 2130968578;
+			
+			// aapt resource value: 0x7f040004
+			public const int takenPictureImageView = 2130968580;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
