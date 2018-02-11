a00a2bfd4994254376ae8c607c92805acb6cd975
jwalker884 <jww193@humboldt.edu>
2018-02-11 01:29:27 -0800

    Added new features to app
    Realized I didn't add any files in my last commit (woops)
    Added a speech-to-text feature, with accompanying record button, and textview text ouput
    Added updated additions from CameraExample, camera function stopped working in debug android emulator and device

---
+++ MainActivity.cs
@@ -0,0 +1,194 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System.Collections.Generic;
+using Android.Content.PM;
+using Android.Provider;
+using Android.Speech;
+
+namespace MultiFuncApp
+{
+
+
+    // IF A CHANGE IS REGISTERED HERE THEN THIS VERSION IS UP TO DATE
+
+
+    [Activity(Label = "MultiFuncApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        /// <summary>
+        /// Used to track the file that we're manipulating between functions
+        /// </summary>
+        public static Java.IO.File _file;
+
+        /// <summary>
+        /// Used to track the directory that we'll be writing to between functions
+        /// </summary>
+        public static Java.IO.File _dir;
+
+        // variabes for our speech to text function
+        private bool isRecording;
+        private readonly int VOICE = 10;
+        private TextView textBox;
+        private Button recButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            if (IsThereAnAppToTakePictures() == true)
+            {
+                CreateDirectoryForPictures();
+                FindViewById<Button>(Resource.Id.launchCameraButton).Click += TakePicture;
+            }
+
+            // SPEECH-TO-TEXT
+            // prompts whether user can record or not 
+            isRecording = false;
+            SetContentView(Resource.Layout.Main);
+
+            recButton = FindViewById<Button>(Resource.Id.recordButton);
+            textBox = FindViewById<TextView>(Resource.Id.textInput);
+
+            // checks to see if user has microphone
+            string rec = Android.Content.PM.PackageManager.FeatureMicrophone;
+            if (rec != "android.hardware.microphone")
+            {
+                // prompts alert if microphone is not found
+                var alert = new AlertDialog.Builder(recButton.Context);
+                alert.SetTitle("No microphone detected");
+                alert.SetPositiveButton("OK", (sender, e) =>
+                {
+                    textBox.Text = "No microphone present";
+                    recButton.Enabled = false;
+                    return;
+                });
+
+                alert.Show();
+            }
+            else
+                recButton.Click += delegate
+                {
+                    recButton.Text = "End voice recording";
+                    isRecording = !isRecording;
+                    if (isRecording)
+                    {
+                        var voiceIntent = new Intent(RecognizerIntent.ActionRecognizeSpeech);
+                        voiceIntent.PutExtra(RecognizerIntent.ExtraLanguageModel, RecognizerIntent.LanguageModelFreeForm);
+
+                        // put a message on the modal dialog
+                        voiceIntent.PutExtra(RecognizerIntent.ExtraPrompt, Application.Context.GetString(Resource.String.messageSpeakNow));
+
+                        // if there is more then 1.5s of silence, consider the speech over
+                        voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputCompleteSilenceLengthMillis, 1500);
+                        voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputPossiblyCompleteSilenceLengthMillis, 1500);
+                        voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputMinimumLengthMillis, 15000);
+                        voiceIntent.PutExtra(RecognizerIntent.ExtraMaxResults, 1);
+                    }
+                };
+        }
+
+        /// <summary>
+        /// Apparently, some android devices do not have a camera.  To guard against this,
+        /// we need to make sure that we can take pictures before we actually try to take a picture.
+        /// </summary>
+        /// <returns></returns>
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities =
+                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        /// <summary>
+        /// Creates a directory on the phone that we can place our images
+        /// </summary>
+        private void CreateDirectoryForPictures()
+        {
+            _dir = new Java.IO.File(
+                Android.OS.Environment.GetExternalStoragePublicDirectory(
+                    Android.OS.Environment.DirectoryPictures), "CameraExample");
+            if (!_dir.Exists())
+            {
+                _dir.Mkdirs();
+            }
+        }
+
+        private void TakePicture(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            _file = new Java.IO.File(_dir, string.Format("myPhoto_{0}.jpg", System.Guid.NewGuid()));
+            //android.support.v4.content.FileProvider
+            //getUriForFile(getContext(), "com.mydomain.fileprovider", newFile);
+            //FileProvider.GetUriForFile;
+
+            //The line is a problem line for Android 7+ development
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(_file));
+            StartActivityForResult(intent, 0);
+        }
+
+        // <summary>
+        // Called automatically whenever an activity finishes
+        // </summary>
+        // <param name = "requestCode" ></ param >
+        // < param name="resultCode"></param>
+        // <param name="data"> </param>
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            //Make image available in the gallery
+
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            var contentUri = Android.Net.Uri.FromFile(_file);
+            mediaScanIntent.SetData(contentUri);
+            SendBroadcast(mediaScanIntent);
+
+
+            // Display in ImageView. We will resize the bitmap to fit the display.
+            // Loading the full sized image will consume too much memory
+            // and cause the application to crash.
+            ImageView imageView = FindViewById<ImageView>(Resource.Id.takenPictureImageView);
+            int height = Resources.DisplayMetrics.HeightPixels;
+            int width = imageView.Height;
+            Android.Graphics.Bitmap bitmap = _file.Path.LoadAndResizeBitmap(width, height);
+            if (bitmap != null)
+            {
+                imageView.SetImageBitmap(bitmap);
+                imageView.Visibility = Android.Views.ViewStates.Visible;
+                bitmap = null;
+            }
+
+            // Dispose of the Java side bitmap
+            System.GC.Collect();
+
+            // for speech-to-text activity
+            if (requestCode == VOICE)
+            {
+                if (resultCode == Result.Ok)
+                {
+                    var matches = data.GetStringArrayListExtra(RecognizerIntent.ExtraResults);
+                    if (matches.Count != 0)
+                    {
+                        string textInput = textBox.Text + matches[0];
+
+                        if (textInput.Length > 500)
+                            textInput = textInput.Substring(0, 500);
+                        textBox.Text = textInput;
+                    }
+                    else
+                        textBox.Text = "No speech recognized";
+                    recButton.Text = "Start Recording";
+                }
+            }
+            base.OnActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
+
+
