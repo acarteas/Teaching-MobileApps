09ca90bbdf343b01ad208bb02b092dbdeab0ab24
chris <chrishj@gmail.com>
2018-02-10 16:57:06 -0800

    fixed scrolling buttons in second view

--- projects/project 2/source/Image Manipulator/Image Manipulator/Resources/Resource.Designer.cs
+++ projects/project 2/source/Image Manipulator/Image Manipulator/Resources/Resource.Designer.cs
@@ -50,26 +50,23 @@ namespace Image_Manipulator
 			// aapt resource value: 0x7f050000
 			public const int btn_take_picture = 2131034112;
 			
-			// aapt resource value: 0x7f050005
-			public const int button1 = 2131034117;
-			
-			// aapt resource value: 0x7f050006
-			public const int button2 = 2131034118;
-			
-			// aapt resource value: 0x7f050009
-			public const int button3 = 2131034121;
-			
-			// aapt resource value: 0x7f050007
-			public const int button4 = 2131034119;
-			
 			// aapt resource value: 0x7f050004
-			public const int button5 = 2131034116;
+			public const int button1 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int button2 = 2131034117;
 			
 			// aapt resource value: 0x7f050008
-			public const int button6 = 2131034120;
+			public const int button3 = 2131034120;
+			
+			// aapt resource value: 0x7f050006
+			public const int button4 = 2131034118;
 			
 			// aapt resource value: 0x7f050003
-			public const int scrollView1 = 2131034115;
+			public const int button5 = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int button6 = 2131034119;
 			
 			// aapt resource value: 0x7f050002
 			public const int view_image = 2131034114;
