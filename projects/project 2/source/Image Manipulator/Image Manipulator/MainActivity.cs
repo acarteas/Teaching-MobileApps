d1780a8acf6ca6ed69f2e8bb045d4c6d5de9c286
chris <chrishj@gmail.com>
2018-02-10 22:39:34 -0800

    Added first three effects to first four buttons. Button 1 reverts the image to original. Button 2 removes red. Button 3 removes green. Button 4 removes blue. There are no labels for the buttons yet and I still need to edit the imagebutton images to show their effect.

--- projects/project 2/source/Image Manipulator/Image Manipulator/MainActivity.cs
+++ projects/project 2/source/Image Manipulator/Image Manipulator/MainActivity.cs
@@ -8,20 +8,12 @@ using Android.Provider;
 
 namespace Image_Manipulator
 {
-    [Activity(Label = "Image_Manipulator", MainLauncher = true, Icon = "@mipmap/icon")]
+    [Activity(Label = "Image Manipulator", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
         string filepath;
-        /// <summary>
-        /// Used to track the file that we're manipulating between functions
-        /// </summary>
         public static Java.IO.File _file;
-
-        /// <summary>
-        /// Used to track the directory that we'll be writing to between functions
-        /// </summary>
         public static Java.IO.File _dir;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -36,11 +28,6 @@ namespace Image_Manipulator
             }
         }
 
-        /// <summary>
-        /// Apparently, some android devices do not have a camera.  To guard against this,
-        /// we need to make sure that we can take pictures before we actually try to take a picture.
-        /// </summary>
-        /// <returns></returns>
         private bool IsThereAnAppToTakePictures()
         {
             Intent intent = new Intent(MediaStore.ActionImageCapture);
@@ -50,9 +37,6 @@ namespace Image_Manipulator
             return availableActivities != null && availableActivities.Count > 0;
         }
 
-        /// <summary>
-        /// Creates a directory on the phone that we can place our images
-        /// </summary>
         private void CreateDirectoryForPictures()
         {
             _dir = new Java.IO.File(
@@ -68,39 +52,18 @@ namespace Image_Manipulator
         {
             Intent intent = new Intent(MediaStore.ActionImageCapture);
             _file = new Java.IO.File(_dir, string.Format("myPhoto_{0}.jpg", System.Guid.NewGuid()));
-            //android.support.v4.content.FileProvider
-            //getUriForFile(getContext(), "com.mydomain.fileprovider", newFile);
-            //FileProvider.GetUriForFile
-
-            //The line is a problem line for Android 7+ development
-            //intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(_file));
             filepath = _file.AbsolutePath;
             StartActivityForResult(intent, 0);
         }
 
-        // <summary>
-        // Called automatically whenever an activity finishes
-        // </summary>
-        // <param name = "requestCode" ></ param >
-        // < param name="resultCode"></param>
-        /// <param name="data"></param>
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
             Android.Graphics.Bitmap bitmap = (Android.Graphics.Bitmap)data.Extras.Get("data");
-           // Android.Graphics.Bitmap copyBitmap =
-             //   bitmap.Copy(Android.Graphics.Bitmap.Config.Argb8888, true);
-
-
-
-
-
 
             var to_send = new Intent(this, typeof(ViewImage));
-
             to_send.PutExtra("image", bitmap);
             StartActivity(to_send);
-
 
 
             //Make image available in the gallery
--- projects/project 2/source/Image Manipulator/Image Manipulator/view.cs
+++ projects/project 2/source/Image Manipulator/Image Manipulator/view.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using Android.Graphics;
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 
 namespace Image_Manipulator
@@ -15,27 +9,124 @@ namespace Image_Manipulator
     [Activity(Label = "ViewImage")]
     public class ViewImage : Activity
     {
+        void copy_bitmap(Bitmap original, Bitmap copy)
+        {
+            for (int i = 0; i < original.Width; i++)
+            {
+                for (int j = 0; j < original.Height; j++)
+                {
+                    int p = original.GetPixel(i, j);
+                    Color c = new Color(p);
+                    copy.SetPixel(i, j, c);
+                }
+            }
+        }
+
+        void revert_to_original (Bitmap original, Bitmap bitmap)
+        {
+            for (int i = 0; i < original.Width; i++)
+            {
+                for (int j = 0; j < original.Height; j++)
+                {
+                    int p = original.GetPixel(i, j);
+                    Color c = new Color(p);
+                    bitmap.SetPixel(i, j, c);
+                }
+            }
+        }
+
+        void remove_red (Bitmap image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    int p = image.GetPixel(i, j);
+                    Color c = new Color(p);
+                    c.R = 0;
+                    image.SetPixel(i, j, c);
+                }
+            }
+        }
+
+        void remove_green(Bitmap image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    int p = image.GetPixel(i, j);
+                    Color c = new Color(p);
+                    c.G = 0;
+                    image.SetPixel(i, j, c);
+                }
+            }
+        }
+
+        void remove_blue(Bitmap image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    int p = image.GetPixel(i, j);
+                    Color c = new Color(p);
+                    c.B = 0;
+                    image.SetPixel(i, j, c);
+                }
+            }
+        }
+
+
+
+
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.view);
-            // Display in ImageView. We will resize the bitmap to fit the display.
-            // Loading the full sized image will consume too much memory
-            // and cause the application to crash.
+
+            //set up image input and make a copy
             ImageView imageView = FindViewById<ImageView>(Resource.Id.view_image);
-            //int height = Resources.DisplayMetrics.HeightPixels;
-            //int width = imageView.Height;
-
-            //AC: workaround for not passing actual files
-            //Android.Graphics.Bitmap bitmap = (Android.Graphics.Bitmap)data.Extras.Get("data");
-            //Android.Graphics.Bitmap copyBitmap =
-            //    bitmap.Copy(Android.Graphics.Bitmap.Config.Argb8888, true);
-
-            //this code removes all red from a picture
-
-            Android.Graphics.Bitmap bitmap = (Android.Graphics.Bitmap)Intent.GetParcelableExtra("image");
-
+            Bitmap bitmap = (Bitmap)Intent.GetParcelableExtra("image");
             imageView.SetImageBitmap(bitmap);
+            Bitmap original_copy = Bitmap.CreateBitmap(bitmap.Width, bitmap.Height, bitmap.GetConfig());
+            copy_bitmap(bitmap, original_copy);
+
+
+            //buttons
+            ImageButton btn_no_effect = FindViewById<ImageButton>(Resource.Id.button_no_effect);
+            ImageButton btn_remove_red = FindViewById<ImageButton>(Resource.Id.button_remove_red);
+            ImageButton btn_remove_green = FindViewById<ImageButton>(Resource.Id.button_remove_green);
+            ImageButton btn_remove_blue = FindViewById<ImageButton>(Resource.Id.button_remove_blue);
+
+            btn_no_effect.Click += delegate
+            {
+                //bitmap = Bitmap.CreateBitmap(original.Width, original.Height, original.GetConfig());
+                copy_bitmap(original_copy, bitmap);
+                imageView.SetImageBitmap(bitmap);
+            };
+
+            btn_remove_red.Click += delegate
+            {
+                remove_red(bitmap);
+                imageView.SetImageBitmap(bitmap);
+            };
+
+            btn_remove_green.Click += delegate
+            {
+                remove_green(bitmap);
+                imageView.SetImageBitmap(bitmap);
+            };
+
+            btn_remove_blue.Click += delegate
+            {
+                remove_blue(bitmap);
+                imageView.SetImageBitmap(bitmap);
+            };
+
+
             /*
             Android.Graphics.Bitmap copyBitmap =
                 bitmap.Copy(Android.Graphics.Bitmap.Config.Argb8888, true);
