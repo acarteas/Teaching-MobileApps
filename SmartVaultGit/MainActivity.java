9617d990e3dd736a7f6735e299e8cc1eb5d9d704
jwalker884 <jww193@humboldt.edu>
2018-04-02 02:15:45 -0700

    - New changes to primary files for SmartVaultApp
    - Added a cancel button on login screen to end session, added a simple check for correct username and password queries, and added a security counter to check for times credentials are inputted incorrectly

--- SmartVaultGit/MainActivity.java
+++ SmartVaultGit/MainActivity.java
@@ -1,23 +1,85 @@
-package com.example.jww193.vaultapp;
+package com.example.jww193.smartvaultapp2;
 
 import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button loginButtonActivity;
+    Button loginButton, cancelButton;
+    EditText userField, passField;
+    TextView theAttempts, theLeftovers;
+    int counter = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginButtonActivity = (Button) findViewById(R.id.loginButton);
-    }
 
-    public void loginButtonClick(View v) {
+        loginButton = (Button)findViewById(R.id.loginButton);
+        cancelButton = (Button)findViewById(R.id.cancelButton);
+        userField = (EditText)findViewById(R.id.theUsername);
+        passField = (EditText)findViewById(R.id.thePassword);
+        theAttempts = (TextView)findViewById(R.id.attempts);
+        theLeftovers = (TextView)findViewById(R.id.leftovers);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(this, PasswordActivity.class);
+                if(userField.getText().toString().equals("admin") &&
+                        passField.getText().toString().equals("password"))
+                {
+                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                    startActivity(intent);
+                }
+                else
+                    {
+                    Toast.makeText(getApplicationContext(), "Incorrect Credentials", Toast.LENGTH_SHORT).show();
+                    theLeftovers.setVisibility(View.VISIBLE);
+                    theLeftovers.setBackgroundColor(Color.RED);
+                    counter--;
+                    theLeftovers.setText(Integer.toString(counter));
+
+                    if(counter == 0)
+                    {
+                        loginButton.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
+    }
+}
+
+
+
+
+
+    /* public void loginButtonClick(View v)
+    {
         Intent intent = new Intent(this, PasswordActivity.class);
         startActivity(intent);
     }
-}
+
+    EditText username = (EditText)findViewById(R.id.theUsername);
+    EditText password = (EditText)findViewById(R.id.thePassword);
+
+    public void login(View view)
+    {
+        if(username.getText().toString().equals("admin") && password.getText().toString().equals("password:"))
+    } */
 
--- SmartVaultGit/PasswordActivity.java
+++ SmartVaultGit/PasswordActivity.java
@@ -1,17 +1,32 @@
-package com.example.jww193.vaultapp;
+package com.example.jww193.smartvaultapp2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.content.Intent;
+import android.widget.Toast;
 import android.widget.Button;
-
+import android.content.Intent;
 
 public class PasswordActivity extends AppCompatActivity {
-
+    Button openVaultButton;
+    Button closeVaultButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.password_main);
+
+        openVaultButton = (Button)findViewById(R.id.openVault);
+        closeVaultButton = (Button)findViewById(R.id.closeVault);
     }
+
+    public void openVaultClick(View v)
+    {
+        Toast.makeText(this, "The Vault has been unlocked!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void closeVaultClick(View v)
+    {
+        Toast.makeText(this , "The Vault has been locked!", Toast.LENGTH_SHORT).show();
+    }
+
 }
